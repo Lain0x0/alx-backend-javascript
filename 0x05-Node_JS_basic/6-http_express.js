524273c5b9bd3d5e877106befe5a174f7a73a8d6
Lain0x0 <lainyamato03@gmail.com>
2024-10-01 21:15:52 +0000

    6-http_express.js

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,20 @@
+/**
+ * Using express in node js to create an HTTP Server
+ */
+const express = require('express');// Import the Express framework
+
+const app = express();// Create an instance of the Express application
+
+// Define a route for the root URL ('/')
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');// Send response when this route is accessed
+});
+
+// Start & listen to the server on 1245 port
+app.listen(1245, () => {
+  console.log('Server listening on port 1245');// Log a message when the server starts
+});
+
+
+// module export 
+module.exports = app;
