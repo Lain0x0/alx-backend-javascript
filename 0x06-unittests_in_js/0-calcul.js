b9ac64de43b692cafb4116a96caf3287de9b99b6
Lain0x0 <lainyamato03@gmail.com>
2024-10-03 16:28:29 +0000

    0-calcul-tese.js

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,10 @@
+/**
+ * Calculing the round of a & b and testing
+ * using assert library on mocha
+*/
+
+function calculateNumber(a, b){
+    return Math.round(a) + Math.round(b);
+}
+
+module.exports = calculateNumber;
