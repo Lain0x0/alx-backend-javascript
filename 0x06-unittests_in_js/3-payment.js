da45e2dca5ee3b50268fae0b054b24fbb4bb1fbd
Lain0x0 <lainyamato03@gmail.com>
2024-10-03 16:52:51 +0000

    3-payment-test

---
+++ 0x06-unittests_in_js/3-payment.js
@@ -0,0 +1,12 @@
+/**
+ * Inherate from other code to do arthmitics operations
+ */
+
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${result}`);
+}
+
+module.exports = sendPaymentRequestToApi;
